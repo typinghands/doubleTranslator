fe6d18ea68ffd86826a02e22b3e88a9b74580e77
typinghands <samsy716@gmail.com>
2019-03-18 01:23:53 +0100

    translation initially working

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ function bookSearch() {
   console.log(searchResult3);
 
   $.ajax({
-    url: `https://www.googleapis.com/books/v1/volumes?q=${searchResult}`,
+    url: `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20140720T191145Z.05605441c6ee16dc.eaaf6c6c8690cb5fb094cea2bfec4f787af6170c&lang=en-de&text=${searchResult}`,
     dataType: "json",
     success(data) {
       console.log(data);
-      document.getElementById("searchBar2").value = searchResult;
+      document.getElementById("searchBar2").value = data.text;
       document.getElementById("searchBar4").value = searchResult3;
     },
     error() {
