5311ddfdf27ee73569439dbdbaf6f493a36c1a64
typinghands <samsy716@gmail.com>
2019-03-18 22:28:38 +0100

    simplified heavily the copy to clipboard and now cleans spaces

--- main.js
+++ main.js
@@ -119,59 +119,27 @@ function deleteAllResults() {
   document.getElementById("bookContainer").innerHTML = "";
 }
 
-function copyToClipboard(elem) {
-  // create hidden text element, if it doesn't already exist
-  const targetId = "_hiddenCopyText_";
-  const isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
-  let origSelectionStart;
-  let origSelectionEnd;
-  if (isInput) {
-    // can just use the original source element for the selection and copy
-    target = elem;
-    origSelectionStart = elem.selectionStart;
-    origSelectionEnd = elem.selectionEnd;
-  } else {
-    // must use a temporary form element for the selection and copy
-    target = document.getElementById(targetId);
-    if (!target) {
-      var target = document.createElement("textarea");
-      target.style.position = "absolute";
-      target.style.left = "-9999px";
-      target.style.top = "0";
-      target.id = targetId;
-      document.body.appendChild(target);
-    }
-    target.textContent = elem.textContent;
-  }
-  // select the content
-  const currentFocus = document.activeElement;
-  target.focus();
-  target.setSelectionRange(0, target.value.length);
-
-  // copy the selection
-  let succeed;
-  try {
-    succeed = document.execCommand("copy");
-  } catch (e) {
-    succeed = false;
-  }
-  // restore original focus
-  if (currentFocus && typeof currentFocus.focus === "function") {
-    currentFocus.focus();
-  }
-
-  if (isInput) {
-    // restore prior selection
-    elem.setSelectionRange(origSelectionStart, origSelectionEnd);
-  } else {
-    // clear temporary content
-    target.textContent = "";
-  }
-  return succeed;
+function copyToClipboard(str) {
+  // Create new element
+  const el = document.createElement("textarea");
+  // Set value (string to be copied)
+  el.value = str;
+  // Set non-editable to avoid focus and move outside of view
+  el.setAttribute("readonly", "");
+  el.style = { position: "absolute", left: "-9999px" };
+  document.body.appendChild(el);
+  // Select text inside element
+  el.select();
+  // Copy text to clipboard
+  document.execCommand("copy");
+  // Remove temporary element
+  document.body.removeChild(el);
 }
 
 function copyAllResults() {
-  copyToClipboard(document.getElementById("bookContainer"));
+  const storedClip = document.getElementById("bookContainer").innerHTML;
+  const noSpaceString = storedClip.replace(/\s\s+/g, " ");
+  copyToClipboard(noSpaceString);
 }
 
 function fnDoublecheckResults() {
