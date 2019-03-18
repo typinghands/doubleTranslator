93d824b5fa5fa4c5e1eb73e9d2217488f6c435e4
typinghands <samsy716@gmail.com>
2019-03-18 01:28:17 +0100

    basic two way translation working

--- main.js
+++ main.js
@@ -10,12 +10,26 @@ function bookSearch() {
     success(data) {
       console.log(data);
       document.getElementById("searchBar2").value = data.text;
-      document.getElementById("searchBar4").value = searchResult3;
     },
     error() {
-      alert(
+      /*    alert(
         "There was an error accessing the API. Please try refreshing the page."
-      );
+      ); */
+    },
+    type: "GET"
+  });
+
+  $.ajax({
+    url: `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20140720T191145Z.05605441c6ee16dc.eaaf6c6c8690cb5fb094cea2bfec4f787af6170c&lang=de-en&text=${searchResult3}`,
+    dataType: "json",
+    success(data) {
+      console.log(data);
+      document.getElementById("searchBar4").value = data.text;
+    },
+    error() {
+      /*  alert(
+        "There was an error accessing the API. Please try refreshing the page."
+      ); */
     },
     type: "GET"
   });
